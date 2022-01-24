edea71b2fd5036dbbfa9287755551064c4bce348
ABoni14 <aboni150@gmail.com>
2022-01-24 15:02:28 +0100

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,13 @@
+<header class="container py-5">
+  <ul class="nav nav-pills">
+     <li class="nav-item">
+       <a class="nav-link">Home</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link">Attori</a>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link">Films</a>
+     </li>
+  </ul>
+</header>
